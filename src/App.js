9f6c1a72233e7c70e39f0f60bec96e4e440c34f0
히지니 <potato9417@gmail.com>
2020-06-22 17:09:33 +0900

    배열을 이용한 react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-function Food({name}){
-  console.log(name)
-  return <h3>I Love {name}</h3>
+function Animal({name,image}){
+  return <div>
+    <h3>I love {name}</h3>
+    <img src={image} />
+  </div>
 }
 
 // function Food(props){
@@ -10,14 +12,22 @@ function Food({name}){
 //   return <h3>I Love {props.name}</h3>
 // }
 
+
+const animalILike = [
+  {name: "Cat", image: "https://topclass.chosun.com/news_img/1807/1807_008_1.jpg"},
+  {name: "Dog", image: "https://i.ytimg.com/vi/ylPccmfeSGM/maxresdefault.jpg"},
+  {name: "Hedgehog", image: "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/2D9/image/X82UKYOxRL9pKoej0FHW8cxrkS8.jpg"},
+  {name: "Hamster", image: "https://i.pinimg.com/originals/78/95/a1/7895a14ce3659f68a4a09aaae260732a.jpg"},
+  {name: "Rabbit", image: "https://i.fltcdn.net/contents/954/original_1420186557146_su59hk6gvi.jpeg"}
+]
+
 function App() {
   return (
     <div>
       <h1>My favorite food~</h1>
-      <Food name="pizza" />
-      <Food name="kimbap" />
-      <Food name="ddokbokki" />
-      <Food name="ramen" />
+      {animalILike.map(animal=>
+        <Animal name={animal.name} image={animal.image} />
+      )}  
 
     </div>
   );
