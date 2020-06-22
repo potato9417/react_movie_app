576a127d354cc8de13ef815dafb5b9cc070de56c
히지니 <potato9417@gmail.com>
2020-06-22 12:52:35 +0900

    props를 이용하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import Potato from "./Potato";
+
+function Food({name}){
+  console.log(name)
+  return <h3>I Love {name}</h3>
+}
+
+// function Food(props){
+//   console.log(props)
+//   return <h3>I Love {props.name}</h3>
+// }
 
 function App() {
   return (
     <div>
-      <h1>리액트는?</h1>
-      <Potato />
+      <h1>My favorite food~</h1>
+      <Food name="pizza" />
+      <Food name="kimbap" />
+      <Food name="ddokbokki" />
+      <Food name="ramen" />
+
     </div>
   );
 }
