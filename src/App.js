5b30054b6da3e343781054724c0cd90470e07ea8
히지니 <potato9417@gmail.com>
2020-06-23 11:21:48 +0900

    class component의 기초

--- src/App.js
+++ src/App.js
@@ -15,31 +15,27 @@ function Animal({name,image,rating}){
 // }
 
 
-const animalILike = [
-  {id: 1, name: "Cat", image: "https://topclass.chosun.com/news_img/1807/1807_008_1.jpg", rating: 4.6},
-  {id: 2, name: "Dog", image: "https://i.ytimg.com/vi/ylPccmfeSGM/maxresdefault.jpg", rating: 4.2},
-  {id: 3, name: "Hedgehog", image: "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/2D9/image/X82UKYOxRL9pKoej0FHW8cxrkS8.jpg", rating: 4.8},
-  {id: 4, name: "Hamster", image: "https://i.pinimg.com/originals/78/95/a1/7895a14ce3659f68a4a09aaae260732a.jpg", rating: 3.9},
-  {id: 5, name: "Rabbit", image: "https://i.fltcdn.net/contents/954/original_1420186557146_su59hk6gvi.jpeg", rating: 3.2}
-]
+class App extends React.Component {
+  state={
+    number :0
+  }
+  plus=()=>{
+    console.log("plus")
+  }
+  minus=()=>{
+    console.log("minus");
+  }
+  render(){
+    // render은 method
+    return (
+      <div>
+        <h1>The number is : {this.state.number}</h1>
+        <button onClick={this.plus}>PLUS</button>
+        <button onClick={this.minus}>MINUS</button>
+      </div>
+    )
+  }
 
-// https://reactjs.org/docs/typechecking-with-proptypes.html
-Animal.propTypes={
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
-
-function App() {
-  return (
-    <div>
-      <h1>My favorite animal~</h1>
-      {animalILike.map(animal=>
-        <Animal key={animal.id} name={animal.name} image={animal.image} rating={animal.rating} />
-      )}  
-
-    </div>
-  );
 }
 
 export default App;
