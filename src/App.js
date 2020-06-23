075c6e37abad7f5979ece65aab74a04cd13356b7
히지니 <potato9417@gmail.com>
2020-06-23 16:53:10 +0900

    영화 관련된부분만 json가져오기

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ class App extends React.Component {
     isLoading :true
   }
   getMovies = async ()=>{
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+    const {data:{data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+    console.log({movies})
+    this.setState({movies,isLoading:false})
   } 
   componentDidMount(){
     this.getMovies()
-    
   }
   render(){
     const isLoading = this.state.isLoading
