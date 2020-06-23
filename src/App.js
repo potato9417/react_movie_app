dbe92e820c30db3f9080cc93865a7ccde4ab69d8
히지니 <potato9417@gmail.com>
2020-06-23 13:25:00 +0900

    왜커밋안대?

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React from "react";
 
 class App extends React.Component {
-  state={
-    number :0
+  state ={
+    isLoading :true
   }
-  plus=()=>{
-    this.setState(current=>({number : current.number+1}))
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({isLoading:false})
+    },3000)
   }
-  minus=()=>{
-    this.setState({number : this.state.number-1})
-  }
-  // plus함수 minus함수 처럼 두개다 사용가능 하지만 this.state처럼 state에 저장된 값을 불러오는것은 좋지않아
-  // react에서 제공해주는 현재값을 이용하여 사용하는것이 좋다 (plus방법으로 사용하길 권장) 
-
   render(){
-    // render은 method
-    return (
-      <div>
-        <h1>The number is : {this.state.number}</h1>
-        <button onClick={this.plus}>PLUS</button>
-        <button onClick={this.minus}>MINUS</button>
-      </div>
-    )
-  }
-
+    const isLoading = this.state.isLoading
+    return <div>{isLoading ? "Loading.." : "Ready!!"}</div>
+  }        
 }
 
 export default App;
