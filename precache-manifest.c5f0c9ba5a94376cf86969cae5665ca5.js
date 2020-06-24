e9f110eff66b00f69e4bc3d8c40dcbe4a6922fd7
히지니 <potato9417@gmail.com>
2020-06-24 10:37:21 +0900

    Updates

---
+++ precache-manifest.c5f0c9ba5a94376cf86969cae5665ca5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2e322f1306c0974bc360cc5c2ce5ba",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "607d0128a06ef4e0d650",
+    "url": "/react_movie_app/static/css/main.7329e25a.chunk.css"
+  },
+  {
+    "revision": "21743db7752266b68615",
+    "url": "/react_movie_app/static/js/2.7067e2d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_movie_app/static/js/2.7067e2d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607d0128a06ef4e0d650",
+    "url": "/react_movie_app/static/js/main.31ebe6db.chunk.js"
+  },
+  {
+    "revision": "6f3fdc26253dd591a598",
+    "url": "/react_movie_app/static/js/runtime-main.167966b2.js"
+  }
+]);
